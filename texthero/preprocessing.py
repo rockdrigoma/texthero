4facc7b4b6de61a662d09cce8ef78421327d7f42
Rodrigo Martinez Arzate <rodrigomartinez@predictvia.com>
2020-09-25 20:31:58 -0500

    Fix typo

--- texthero/preprocessing.py
+++ texthero/preprocessing.py
@@ -1307,7 +1307,7 @@ def get_twitter_pipeline() -> List[Callable[[pd.Series], pd.Series]]:
     return [
         fillna,
         replace_emojis,
-        rreplace_urls_w_code,
+        replace_urls_w_code,
         replace_hashtags_w_code,
         replace_mentions_w_code,
         check_spelling,
